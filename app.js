71d76d4012d82c0dc3359159627b7917a1c2db6a
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-09-17 20:14:18 +0100

    feat: Play one game of RPS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function () {
-  const choices = ['Rock', 'Paper', 'Scissors'];
+  const choices = ['ROCK', 'PAPER', 'SCISSORS'];
 
   function computerPlay() {
     return choices[randomNumber()];
@@ -8,6 +8,54 @@
   function randomNumber(max = 3) {
     return Math.floor((Math.random() * max));
   }
+
+  function playRound() {
+    const playerChoice = getPlayerInput();
+    const sanitisedInput = sanitiseInput(playerChoice);
+
+    if (isValidPlayerChoice(sanitisedInput)) {
+      announceResults(sanitisedInput, computerPlay())
+    } else {
+      console.log('Please submit a valid choice');
+      playRound();
+    }
+  }
+
+  function getPlayerInput(message = "What's your choice? Rock, Paper, or Scissors?") {
+    return prompt(message);
+  }
+
+  function sanitiseInput(playerChoice) {
+    return playerChoice.trim().toUpperCase();
+  }
+
+  function isValidPlayerChoice(choice) {
+    return choices.includes(choice)
+  }
+
+  function announceResults(playerChoice, compChoice) {
+    let result = '';
+
+    switch (true) {
+      case (playerChoice === compChoice):
+        result = 'DRAW';
+        break;
+      case (playerChoice === choices[0]):
+        result = compChoice === choices[1] ? 'LOSE' : 'WIN';
+        break;
+      case (playerChoice === choices[1]):
+        result = compChoice === choices[2] ? 'LOSE' : 'WIN';
+        break;
+      case (playerChoice === choices[2]):
+        result = compChoice === choices[0] ? 'LOSE' : 'WIN';
+        break;
+    }
+
+    console.log(result, `\nYou chose: ${playerChoice}\nComputer chose: ${compChoice}`)
+  }
+
+  /* Game begins here */
+  playRound();
 
   /* Testing random distribution */
 
